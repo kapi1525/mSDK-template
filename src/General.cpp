a8fd3c0c540df8c27338613c5045d40547319679
kapigames <kapigamesdev@gmail.com>
2022-06-26 19:13:20 +0200

    Cleanup of General.cpp

--- src/General.cpp
+++ src/General.cpp
@@ -1,242 +1,160 @@
-
-// ============================================================================
-//
-// The following routines are used internally by MMF, and should not need to
-// be modified.
-//
-// 
-// ============================================================================
-
-// Common Include
+// Functions in this file are used internaly by Fusion, you probably dont need to change them.
 #include	"common.h"
 
 HINSTANCE hInstLib;
 
-// ============================================================================
-//
-// LIBRARY ENTRY & QUIT POINTS
-// 
-// ============================================================================
 
-// -----------------
-// Entry points
-// -----------------
-// Usually you do not need to do any initialization here: you will prefer to 
-// do them in "Initialize" found in Edittime.cpp
-BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved)
-{
-	switch (dwReason)
-	{
-		// DLL is attaching to the address space of the current process.
-		case DLL_PROCESS_ATTACH:
-			
-			hInstLib = hDLL; // Store HINSTANCE
-			break;
 
-		// A new thread is being created in the current process.
-		case DLL_THREAD_ATTACH:
-			break;
-
-		// A thread is exiting cleanly.
-		case DLL_THREAD_DETACH:
-			break;
-
-		// The calling process is detaching the DLL from its address space.
-	    case DLL_PROCESS_DETACH:
-			break;
-	}
-	
-	return TRUE;
+// DLL entry point
+BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved) {
+    switch (dwReason) {
+    case DLL_PROCESS_ATTACH:
+        hInstLib = hDLL; 	// Store HINSTANCE, it will be needed when loading resources from rc file.
+        break;
+    
+    default:
+        break;
+    }
+    
+    return TRUE;
 }
 
-// -----------------
-// Initialize
-// -----------------
-// Where you want to do COLD-START initialization.
-// Called when the extension is loaded into memory.
-//
-extern "C" int WINAPI DLLExport Initialize(mv _far *mV, int quiet)
-{
-	// No error
-	return 0;
+
+
+// Called when extension is loaded into memory
+extern "C" int WINAPI DLLExport Initialize(mv* mV, int quiet) {
+    return 0;
 }
 
-// -----------------
-// Free
-// -----------------
-// Where you want to kill and initialized data opened in the above routine
-// Called just before freeing the DLL.
-// 
-extern "C" int WINAPI DLLExport Free(mv _far *mV)
-{
-	// No error
-	return 0;
+// Called just before extension is unloaded
+extern "C" int WINAPI DLLExport Free(mv* mV) {
+    return 0;
 }
 
-// ============================================================================
-//
-// GENERAL INFO
-// 
-// ============================================================================
 
-// -----------------
-// Get Infos
-// -----------------
-// 
-extern "C" 
-{
-	DWORD WINAPI DLLExport GetInfos(int info)
-	{
-		
-		switch (info)
-		{
-			case KGI_VERSION:
-				return EXT_VERSION2;				// Do not change
-			case KGI_PLUGIN:
-				return EXT_PLUGIN_VERSION1;			// Do not change
-			case KGI_PRODUCT:
-#if defined(PROEXT)
-				return PRODUCT_VERSION_DEV;			// Works with MMF Developer 
-#else
-				return PRODUCT_VERSION_STANDARD;	// Works with MMF Standard or above
-#endif
-			case KGI_BUILD:
-				return MINBUILD;					// Works with build MINBUILD or above
-			case KGI_UNICODE:
-				#ifdef _UNICODE
-					return TRUE;
-				#else
-					return FALSE;
-				#endif
-			default:
-				return 0;
-		}
-	}
+
+// This function tells Fusion some basic information about your extension
+extern "C" DWORD WINAPI DLLExport GetInfos(int info) {
+    switch (info)
+    {
+    case KGI_VERSION:
+        return EXT_VERSION2;			// Do not change
+    
+    case KGI_PLUGIN:
+        return EXT_PLUGIN_VERSION1;		// Do not change
+    
+    case KGI_PRODUCT:
+        #ifdef PROEXT
+        return PRODUCT_VERSION_DEV;			// Fusion Developer 
+        #else
+        return PRODUCT_VERSION_STANDARD;	// Fusion Standard and above
+        #endif
+
+    case KGI_BUILD:
+        return MINBUILD;                // Required Fusion build
+    
+    case KGI_UNICODE:
+        #ifdef UNICODE
+        return TRUE;
+        #else
+        return FALSE;
+        #endif
+
+    default:
+        return 0;
+    }
 }
 
-// ----------------------------------------------------------
-// GetRunObjectInfos
-// ----------------------------------------------------------
-// Fills an information structure that tells CC&C everything
-// about the object, its actions, conditions and expressions
-// 
-
+// Tells Fusion everything about your extension
+// Actions Condition Expressions etc
 short WINAPI DLLExport GetRunObjectInfos(mv _far *mV, fpKpxRunInfos infoPtr)
 {
-	infoPtr->conditions = (LPBYTE)ConditionJumps;
-	infoPtr->actions = (LPBYTE)ActionJumps;
-	infoPtr->expressions = (LPBYTE)ExpressionJumps;
+    infoPtr->conditions = (LPBYTE)ConditionJumps;
+    infoPtr->actions = (LPBYTE)ActionJumps;
+    infoPtr->expressions = (LPBYTE)ExpressionJumps;
 
-	infoPtr->numOfConditions = CND_LAST;
-	infoPtr->numOfActions = ACT_LAST;
-	infoPtr->numOfExpressions = EXP_LAST;
+    infoPtr->numOfConditions = CND_LAST;
+    infoPtr->numOfActions = ACT_LAST;
+    infoPtr->numOfExpressions = EXP_LAST;
 
-	infoPtr->editDataSize = MAX_EDITSIZE;
-	infoPtr->editFlags= OEFLAGS;
+    infoPtr->editDataSize = MAX_EDITSIZE;
+    infoPtr->editFlags= OEFLAGS;
 
-	infoPtr->windowProcPriority = WINDOWPROC_PRIORITY;
+    infoPtr->windowProcPriority = WINDOWPROC_PRIORITY;
 
-	// See doc
-	infoPtr->editPrefs = OEPREFS;
+    // See doc
+    infoPtr->editPrefs = OEPREFS;
 
-	// Identifier, for run-time identification
-	infoPtr->identifier = IDENTIFIER;
-	
-	// Current version
-	infoPtr->version = KCX_CURRENT_VERSION;
-	
-	return TRUE;
+    // Identifier, for run-time identification
+    infoPtr->identifier = IDENTIFIER;
+    
+    // Current version
+    infoPtr->version = KCX_CURRENT_VERSION;
+    
+    return TRUE;
 }
 
-// ----------------------------------------------------------
-// GetDependencies
-// ----------------------------------------------------------
-// Returns the name of the external modules that you wish MMF to include
-// with stand-alone applications (these modules must be in the MMF
-// Data\Runtime folder).
-// 
 
-//LPCTSTR szDep[] = {
-//	_T("MyDll.dll"),
-//	NULL
-//};
 
-LPCTSTR* WINAPI DLLExport GetDependencies()
-{
-	return NULL;	// szDep;
+// List of external modules
+LPCTSTR szDep[] = {
+    // _T("MyDll.dll"),
+    NULL
+};
+
+// This function returns names of external modules(eg: DLLs) that should be included with built applications
+LPCTSTR* WINAPI DLLExport GetDependencies() {
+    return szDep;	// szDep;
 }
 
-// -----------------
-// LoadObject
-// -----------------
-// Routine called for each object when the object is read from the MFA file (edit time)
-// or from the CCN or EXE file (run time).
-// You can load data here, reserve memory etc...
-//
-int	WINAPI DLLExport LoadObject(mv _far *mV, LPCSTR fileName, LPEDATA edPtr, int reserved)
-{
-	return 0;
+
+
+// Called for each object when its read from MFA or EXE file
+int	WINAPI DLLExport LoadObject(mv* mV, LPCSTR fileName, LPEDATA edPtr, int reserved) {
+    return 0;
 }
 
-// -----------------
-// UnloadObject
-// -----------------
-// The counterpart of the above routine: called just before the object is
-// deleted from the frame.
-//
-void WINAPI DLLExport UnloadObject(mv _far *mV, LPEDATA edPtr, int reserved)
-{
+// Counter part of above function
+// Called before object is deleted from frame
+void WINAPI DLLExport UnloadObject(mv* mV, LPEDATA edPtr, int reserved) {
 }
 
-// --------------------
-// UpdateEditStructure
-// --------------------
-// For you to update your object structure to newer versions
-// Called at both edit time and run time
-// 
-HGLOBAL WINAPI DLLExport UpdateEditStructure(mv __far *mV, void __far * OldEdPtr)
-{
-	// We do nothing here
-	return 0;
+
+
+// Called when objects editdata must be updated to neewer version
+// OldEdPtr is old editdata, extension header is allways first use it to get old editdata version
+// Called both in editor and at runtime
+HGLOBAL WINAPI DLLExport UpdateEditStructure(mv __far *mV, void __far * OldEdPtr) {
+    // We do nothing here
+    return 0;
 }
 
-// --------------------
-// UpdateFileNames
-// --------------------
-// If you store file names in your datazone, they have to be relocated when the
-// application is moved to a different directory: this routine does it.
-// Called at edit time and run time.
-//
-// Call lpfnUpdate to update your file pathname (refer to the documentation)
-// 
+// If you store file names in editdata they have to be updated when application is moved to different directory
+// Call lpfnUpdate() to update them
+// Called both in editor and at runtime
 void WINAPI DLLExport UpdateFileNames(mv _far *mV, LPTSTR appName, LPEDATA edPtr, void (WINAPI * lpfnUpdate)(LPTSTR, LPTSTR))
 {
 }
 
-// ---------------------
-// EnumElts
-// ---------------------
-//
-// Uncomment this function if you need to store an image in the image bank.
-//
+
+
+// Uncomment this function if you need to store an image in editdata
 // Note: do not forget to enable the function in the .def file 
 // if you remove the comments below.
-//
 /*
 int WINAPI DLLExport EnumElts (mv __far *mV, LPEDATA edPtr, ENUMELTPROC enumProc, ENUMELTPROC undoProc, LPARAM lp1, LPARAM lp2)
 {  
-	int error = 0;
+    int error = 0;
 
-	// Replace wImgIdx with the name of the WORD variable you create within the edit structure
+    // Replace wImgIdx with the name of the WORD variable you create within the edit structure
   
-	// Enum images  
-	if ( (error = enumProc(&edPtr->wImgIdx, IMG_TAB, lp1, lp2)) != 0 )
-	{
-		// Undo enum images      
-		undoProc (&edPtr->wImgIdx, IMG_TAB, lp1, lp2);    
-	}  
+    // Enum images  
+    if ( (error = enumProc(&edPtr->wImgIdx, IMG_TAB, lp1, lp2)) != 0 )
+    {
+        // Undo enum images      
+        undoProc (&edPtr->wImgIdx, IMG_TAB, lp1, lp2);    
+    }  
 
-	return error;
+    return error;
 }
 */
-
