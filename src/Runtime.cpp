acb73041af42522fb2e3aa98e4b846aea1da5bd0
kapigames <kapigamesdev@gmail.com>
2022-06-27 02:53:52 +0200

    Finish Runtime.cpp cleanup

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -1,20 +1,14 @@
-// ============================================================================
-//
-// This file contains routines that are handled during the Runtime.
-//
-// Including creating, display, and handling your object.
-// 
-// ============================================================================
+// This file contains functions that are used during runtime
+// eg: Object creation, destruction, drawing object etc.
 
-// Common Include
-#include	"common.h"
+#include "common.h"
 
-// DEBUGGER /////////////////////////////////////////////////////////////////
+
 
 #ifdef EDITTIME
+
 // Identifiers of items displayed in the debugger
-enum
-{
+enum {
 // Example
 // -------
 //	DB_CURRENTSTRING,
@@ -24,8 +18,7 @@ enum
 };
 
 // Items displayed in the debugger
-WORD DebugTree[]=
-{
+WORD DebugTree[] = {
 // Example
 // -------
 //	DB_CURRENTSTRING|DB_EDITABLE,
@@ -40,129 +33,92 @@ WORD DebugTree[]=
 #endif // !defined(RUNTIME)
 
 
-// --------------------
-// GetRunObjectDataSize
-// --------------------
-// Returns the size of the runtime datazone of the object
-// 
-unsigned short WINAPI DLLExport GetRunObjectDataSize(RunHeader* rhPtr, EDITDATA* edPtr) {
-    return(sizeof(RUNDATA));
+
+// Returns size of RUNDATA
+unsigned short FusionAPI GetRunObjectDataSize(RunHeader* rhPtr, EDITDATA* edPtr) {
+    #pragma MFXExport
+
+    return sizeof(RUNDATA);
 }
 
 
-// ---------------
-// CreateRunObject
-// ---------------
-// The routine where the object is actually created
-// 
-short WINAPI DLLExport CreateRunObject(RUNDATA* rdPtr, EDITDATA* edPtr, createObjectInfo* cobPtr) {
-/*
-   This routine runs when your object is created, as you might have guessed.
-   It is here that you must transfer any data you need in rdPtr from edPtr,
-   because after this has finished you cannot access it again!
-   Also, if you have anything to initialise (e.g. dynamic arrays, surface objects)
-   you should do it here, and free your resources in DestroyRunObject.
-*/
-    // No errors
+// Called for each object when its created, each object has its own RUNDATA and EDITDATA
+// Copy everything you need from EDITDATA to RUNDATA because you cannot access it later
+short FusionAPI CreateRunObject(RUNDATA* rdPtr, EDITDATA* edPtr, createObjectInfo* cobPtr) {
+    #pragma MFXExport
+    
+    // OK
+    return 0;
+}
+
+// Called for each object when its destroyed, free any memory you allocated in RUNDATA.
+short FusionAPI DestroyRunObject(RUNDATA* rdPtr, long fast) {
+    #pragma MFXExport
+    
+    // OK
     return 0;
 }
 
 
-// ----------------
-// DestroyRunObject
-// ----------------
-// Destroys the run-time object
-// 
-short WINAPI DLLExport DestroyRunObject(RUNDATA* rdPtr, long fast) {
-/*
-   When your object is destroyed (either with a Destroy action or at the end of
-   the frame) this routine is called. You must free any resources you have allocated!
-*/
-    // No errors
-    return 0;
-}
 
+// Called every loop for each object
+// If you dont do anything in here return 
+short FusionAPI HandleRunObject(RUNDATA* rdPtr) {
+    #pragma MFXExport
+    
+    // If your extension will draw something to screen you should
+    // check if anything about its display has changed
+    // if(rdPtr->rc.rcChanged) {
+    //     return REFLAG_DISPLAY;
+    // } else {
+    //     return 0;
+    // }
 
-// ----------------
-// HandleRunObject
-// ----------------
-// Called (if you want) each loop, this routine makes the object live
-// 
-short WINAPI DLLExport HandleRunObject(RUNDATA* rdPtr) {
-/*
-   If your extension will draw to the MMF window you should first 
-   check if anything about its display has changed :
+    // You will need to change this flag whenever you want to redraw your object
+    // rdPtr->rc.rcChanged = 1;
 
-       if (rdPtr->roc.rcChanged)
-          return REFLAG_DISPLAY;
-       else
-          return 0;
+    // If your extension wont draw anything but still needs to do something each loop use:
+    // return 0;
 
-   You will also need to make sure you change this flag yourself 
-   to 1 whenever you want to redraw your object
- 
-   If your extension won't draw to the window, but it still needs 
-   to do something every MMF loop use :
+    // If you dont use:
+    // return REFLAG_ONESHOT;
 
-        return 0;
-
-   If you don't need to do something every loop, use :
-
-        return REFLAG_ONESHOT;
-
-   This doesn't mean this function can never run again. If you want MMF
-   to handle your object again (causing this code to run) use this function:
-
-        callRun-timeFunction(rdPtr, RFUNCTION_REHANDLE, 0, 0);
-
-   At the end of the loop this code will run
-*/
-    // Will not be called next loop	
+    // If you return REFLAG_ONESHOT you still can handle your object, call this:
+    // callRun-timeFunction(rdPtr, RFUNCTION_REHANDLE, 0, 0);
+    
+    // Will not be called next loop
     return REFLAG_ONESHOT;
 }
 
-// ----------------
-// DisplayRunObject
-// ----------------
-// Draw the object in the application screen.
-// 
-short WINAPI DLLExport DisplayRunObject(RUNDATA* rdPtr) {
-/*
-   If you return REFLAG_DISPLAY in HandleRunObject this routine will run.
-*/
-    // Ok
+// Called when you return REFLAG_DISPLAY in HandleRunObject
+// Draw to screen surface that you can get using WinGetSurface()
+short FusionAPI DisplayRunObject(RUNDATA* rdPtr) {
+    #pragma MFXExport
+
+    // OK
     return 0;
 }
 
-// -------------------
-// GetRunObjectSurface
-// -------------------
-// Implement this function instead of DisplayRunObject if your extension
-// supports ink effects and transitions. Note: you can support ink effects
-// in DisplayRunObject too, but this is automatically done if you implement
-// GetRunObjectSurface (MMF applies the ink effect to the surface).
-//
-// Note: do not forget to enable the function in the .def file 
-// if you remove the comments below.
+// If you want your extension to support Shaders (Effects) and transistions
+// comment out DisplayRunObject and implement this function instead.
+// NOTE: You can have shaders and transitions with DisplayRunObject but this function will implement this for you.
 /*
-cSurface* WINAPI DLLExport GetRunObjectSurface(RUNDATA* rdPtr) {
+cSurface* FusionAPI GetRunObjectSurface(RUNDATA* rdPtr) {
+    #pragma MFXExport
+    
     return NULL;
 }
 */
 
-// -------------------------
-// GetRunObjectCollisionMask
-// -------------------------
-// Implement this function if your extension supports fine collision mode (OEPREFS_FINECOLLISIONS),
-// Or if it's a background object and you want Obstacle properties for this object.
-//
-// Should return NULL if the object is not transparent.
-//
-// Note: do not forget to enable the function in the .def file 
-// if you remove the comments below.
-//
+
+
+// Impement this function if you support fine collision mode (OEPREFS_FINECOLLISIONS)
+// or if its a background object with obstacle properties.
+// Return NULL if object is not transparent.
 /*
-LPSMASK WINAPI DLLExport GetRunObjectCollisionMask(RUNDATA* rdPtr, LPARAM lParam) {
+LPSMASK FusionAPI GetRunObjectCollisionMask(RUNDATA* rdPtr, LPARAM lParam) {
+    #pragma MFXExport
+
     // Typical example for active objects
     // ----------------------------------
     // Opaque? collide with box
@@ -197,34 +153,31 @@ LPSMASK WINAPI DLLExport GetRunObjectCollisionMask(RUNDATA* rdPtr, LPARAM lParam
 }
 */
 
-// ----------------
-// PauseRunObject
-// ----------------
-// Enters the pause mode
-// 
-short WINAPI DLLExport PauseRunObject(RUNDATA* rdPtr) {
-    // Ok
+
+
+// Called when application is paused.
+short FusionAPI PauseRunObject(RUNDATA* rdPtr) {
+    #pragma MFXExport
+
+    return 0;
+}
+
+// Called when application is unpaused.
+short FusionAPI ContinueRunObject(RUNDATA* rdPtr) {
+    #pragma MFXExport
+
     return 0;
 }
 
 
-// -----------------
-// ContinueRunObject
-// -----------------
-// Quits the pause mode
-//
-short WINAPI DLLExport ContinueRunObject(RUNDATA* rdPtr) {
-    // Ok
-    return 0;
-}
 
-// -----------------
-// SaveRunObject
-// -----------------
-// Saves the object to disk
-// 
 
-BOOL WINAPI SaveRunObject(RUNDATA* rdPtr, HANDLE hf) {
+// Called when application shoud be saved to disc.
+// Eg: When using save action.
+// hf is file handle where you should save data using WinAPI functions.
+BOOL FusionAPI SaveRunObject(RUNDATA* rdPtr, HANDLE hf) {
+    #pragma MFXExport
+
     BOOL bOK = FALSE;
 
     // Save the object's data here
@@ -234,169 +187,130 @@ BOOL WINAPI SaveRunObject(RUNDATA* rdPtr, HANDLE hf) {
     return bOK;
 }
 
-// -----------------
-// LoadRunObject
-// -----------------
-// Loads the object from disk
-// 
-BOOL WINAPI LoadRunObject(RUNDATA* rdPtr, HANDLE hf) {
+// Called when application shoud be loaded from disc.
+// Eg: When using load action.
+// hf is file handle where you should load data using WinAPI functions.
+BOOL FusionAPI LoadRunObject(RUNDATA* rdPtr, HANDLE hf) {
+    #pragma MFXExport
+
     BOOL bOK=FALSE;
 
     // Load the object's data here
 
     // OK
     bOK = TRUE;
-
-    return bOK; 
+    return bOK;
 }
 
 
 
-
-// ============================================================================
-//
-// START APP / END APP / START FRAME / END FRAME routines
-// 
-// ============================================================================
-
-// -------------------
-// StartApp
-// -------------------
 // Called when the application starts or restarts.
 // Useful for storing global data
-// 
-void WINAPI DLLExport StartApp(mv* mV, CRunApp* pApp) {
+void FusionAPI StartApp(mv* mV, CRunApp* pApp) {
+    #pragma MFXExport
+
     // Example
     // -------
     // Delete global data (if restarts application)
-//	CMyData* pData = (CMyData*)mV->mvGetExtUserData(pApp, hInstLib);
-//	if ( pData != NULL )
-//	{
-//		delete pData;
-//		mV->mvSetExtUserData(pApp, hInstLib, NULL);
-//	}
+    // CMyData* pData = (CMyData*)mV->mvGetExtUserData(pApp, hInstLib);
+    // if(pData != NULL) {
+    //     delete pData;
+    //     mV->mvSetExtUserData(pApp, hInstLib, NULL);
+    // }
 }
 
-// -------------------
-// EndApp
-// -------------------
 // Called when the application ends.
-// 
-void WINAPI DLLExport EndApp(mv* mV, CRunApp* pApp) {
+void FusionAPI EndApp(mv* mV, CRunApp* pApp) {
+    #pragma MFXExport
+
     // Example
     // -------
     // Delete global data
-//	CMyData* pData = (CMyData*)mV->mvGetExtUserData(pApp, hInstLib);
-//	if ( pData != NULL )
-//	{
-//		delete pData;
-//		mV->mvSetExtUserData(pApp, hInstLib, NULL);
-//	}
+    // CMyData* pData = (CMyData*)mV->mvGetExtUserData(pApp, hInstLib);
+    // if(pData != NULL) {
+    //     delete pData;
+    //     mV->mvSetExtUserData(pApp, hInstLib, NULL);
+    // }
 }
 
-// -------------------
-// StartFrame
-// -------------------
+
 // Called when the frame starts or restarts.
-// 
-void WINAPI DLLExport StartFrame(mv* mV, DWORD dwReserved, int nFrameIndex) {
+void FusionAPI StartFrame(mv* mV, DWORD dwReserved, int nFrameIndex) {
+    #pragma MFXExport
 }
 
-// -------------------
-// EndFrame
-// -------------------
 // Called when the frame ends.
-// 
-void WINAPI DLLExport EndFrame(mv* mV, DWORD dwReserved, int nFrameIndex) {
+void FusionAPI EndFrame(mv* mV, DWORD dwReserved, int nFrameIndex) {
+    #pragma MFXExport
 }
 
-// ============================================================================
-//
-// TEXT ROUTINES (if OEFLAG_TEXT)
-// 
-// ============================================================================
 
-// -------------------
-// GetRunObjectFont
-// -------------------
+
+/* TEXT STUFF
+
 // Return the font used by the object.
-// 
-/*
+void FusionAPI GetRunObjectFont(RUNDATA* rdPtr, LOGFONT* pLf) {
+    #pragma MFXExport
 
-  // Note: do not forget to enable the functions in the .def file 
-  // if you remove the comments below.
-
-void WINAPI GetRunObjectFont(RUNDATA* rdPtr, LOGFONT* pLf) {
     // Example
     // -------
     // GetObject(rdPtr->m_hFont, sizeof(LOGFONT), pLf);
 }
 
-// -------------------
-// SetRunObjectFont
-// -------------------
 // Change the font used by the object.
-// 
-void WINAPI SetRunObjectFont(RUNDATA* rdPtr, LOGFONT* pLf, RECT* pRc) {
+void FusionAPI SetRunObjectFont(RUNDATA* rdPtr, LOGFONT* pLf, RECT* pRc) {
+    #pragma MFXExport
+
     // Example
     // -------
-//	HFONT hFont = CreateFontIndirect(pLf);
-//	if ( hFont != NULL )
-//	{
-//		if (rdPtr->m_hFont!=0)
-//			DeleteObject(rdPtr->m_hFont);
-//		rdPtr->m_hFont = hFont;
-//		SendMessage(rdPtr->m_hWnd, WM_SETFONT, (WPARAM)rdPtr->m_hFont, FALSE);
-//	}
+    // HFONT hFont = CreateFontIndirect(pLf);
+    // if ( hFont != NULL ) {
+    //     if (rdPtr->m_hFont!=0) {
+    //          DeleteObject(rdPtr->m_hFont);
+    //     }
+    //     rdPtr->m_hFont = hFont;
+    //     SendMessage(rdPtr->m_hWnd, WM_SETFONT, (WPARAM)rdPtr->m_hFont, FALSE);
+    // }
 
 }
 
-// ---------------------
-// GetRunObjectTextColor
-// ---------------------
 // Return the text color of the object.
-// 
-COLORREF WINAPI GetRunObjectTextColor(RUNDATA* rdPtr) {
-    // Example
-    // -------
-    return 0;	// rdPtr->m_dwColor;
+COLORREF FusionAPI GetRunObjectTextColor(RUNDATA* rdPtr) {
+    #pragma MFXExport
+
+    return 0;   // rdPtr->m_dwColor;
 }
 
-// ---------------------
-// SetRunObjectTextColor
-// ---------------------
 // Change the text color of the object.
-// 
-void WINAPI SetRunObjectTextColor(RUNDATA* rdPtr, COLORREF rgb) {
+void FusionAPI SetRunObjectTextColor(RUNDATA* rdPtr, COLORREF rgb) {
+    #pragma MFXExport
+
     // Example
     // -------
     rdPtr->m_dwColor = rgb;
     InvalidateRect(rdPtr->m_hWnd, NULL, TRUE);
 }
+
 */
 
 
-// ============================================================================
-//
-// WINDOWPROC (interception of messages sent to hMainWin and hEditWin)
-//
-// Do not forget to enable the WindowProc function in the .def file if you implement it
-// 
-// ============================================================================
-/*
+
+// INTERCEPTION OF WINDOW MESSAGES
 // Get the pointer to the object's data from its window handle
 // Note: the object's window must have been subclassed with a
 // callRunTimeFunction(rdPtr, RFUNCTION_SUBCLASSWINDOW, 0, 0);
 // See the documentation and the Simple Control example for more info.
-//
+
+/*
 RUNDATA* GetRdPtr(HWND hwnd, LPRH rhPtr) {
     return (RUNDATA*)GetProp(hwnd, (LPCSTR)rhPtr->rh4.rh4AtomRd);
 }
 
 // Called from the window proc of hMainWin and hEditWin.
 // You can intercept the messages and/or tell the main proc to ignore them.
-//
-LRESULT CALLBACK DLLExport WindowProc(LPRH rhPtr, HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProc(LPRH rhPtr, HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam) {
+    #pragma MFXExport
+
     RUNDATA* rdPtr = NULL;
 
     switch (nMsg) {
@@ -426,33 +340,28 @@ LRESULT CALLBACK DLLExport WindowProc(LPRH rhPtr, HWND hWnd, UINT nMsg, WPARAM w
 
     return 0;
 }
+
 */
 
-// ============================================================================
-//
-// DEBUGGER ROUTINES
-// 
-// ============================================================================
 
-// -----------------
-// GetDebugTree
-// -----------------
-// This routine returns the address of the debugger tree
-//
-LPWORD WINAPI DLLExport GetDebugTree(RUNDATA* rdPtr) {
-#ifdef EDITTIME
+
+// DEBUGER
+
+// This function returns address of DebugTree
+WORD* FusionAPI GetDebugTree(RUNDATA* rdPtr) {
+    #pragma MFXExport
+
+    #ifdef EDITTIME
     return DebugTree;
-#else
+    #else
     return NULL;
-#endif // EDITTIME
+    #endif // EDITTIME
 }
 
-// -----------------
-// GetDebugItem
-// -----------------
-// This routine returns the text of a given item.
-//
-void WINAPI DLLExport GetDebugItem(LPTSTR pBuffer, RUNDATA* rdPtr, int id) {
+// This function returns the text of a given item.
+void FusionAPI GetDebugItem(LPTSTR pBuffer, RUNDATA* rdPtr, int id) {
+    #pragma MFXExport
+
 #ifdef EDITTIME
 
     // Example
@@ -487,12 +396,10 @@ void WINAPI DLLExport GetDebugItem(LPTSTR pBuffer, RUNDATA* rdPtr, int id) {
 #endif // EDITTIME
 }
 
-// -----------------
-// EditDebugItem
-// -----------------
-// This routine allows to edit editable items.
-//
-void WINAPI DLLExport EditDebugItem(RUNDATA* rdPtr, int id) {
+// This function allows to edit editable items.
+void FusionAPI EditDebugItem(RUNDATA* rdPtr, int id) {
+    #pragma MFXExport
+    
 #ifdef EDITTIME
 
     // Example
